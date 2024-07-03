4821f5d632d18e8247cf81ca62c4123e110af614
liwrik <vlamukova@gmail.com>
2024-07-03 19:41:16 +0500

    git

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Bird {
         this.acceleration = 0.4
         this.image = new Image()
         this.image.src = "bird.png"
-        this.jump_power = -9
+        this.jump_power = -8
         this.point = 0
     }
     draw() {
@@ -39,11 +39,11 @@ class Bird {
 
 class Pipe{
     constructor(x) {
-        let interval = 200
+        let interval = 180
         let min_h = 50
-        let max_random_h = canvas.height - min_h - interval
+        let max_random_h = canvas.height - min_h - min_h - interval
         
-        let speed = -1.5
+        let speed = -3
         let width = 50
         this.fly = false
 
@@ -52,8 +52,54 @@ class Pipe{
             x: x,
             y: 0,
             width: width,
-            height:min_h + Math.round(Math.random()),
+            height: min_h + Math.round(Math.random() * max_random_h),
             speed: speed,
+        }
+
+        this.down_pipe = {
+            start_: x,
+            x: x,
+            y: this.top_pipe.height + interval, 
+            width: width,
+            height: canvas.height - this.top_pipe.height - interval,
+            speed: speed,
+        }
+
+    }
+
+    draw() {
+        ctx.fillStyle = "#333";
+        ctx.fillRect(
+            this.top_pipe.x,
+            this.top_pipe.y,
+            this.top_pipe.width,
+            this.top_pipe.height,
+        );
+
+        ctx.fillRect(
+            this.down_pipe.x,
+            this.down_pipe.y,
+            this.down_pipe.width,
+            this.down_pipe.height,
+        )
+    }
+
+    move() {
+        this.top_pipe.x += this.top_pipe.speed
+        this.down_pipe.x += this.down_pipe.speed
+
+        if (this.top_pipe.x < -this.top_pipe.width) {
+            this.top_pipe.x = canvas.width
+            this.down_pipe.x = canvas.width
+
+            let interval = 200
+            let min_h = 50
+            let max_random_h = canvas.height - min_h - min_h - interval
+            
+            this.top_pipe.height = min_h + Math.round(Math.random() * max_random_h)
+            this.down_pipe.y = this.top_pipe.height + interval
+            this.down_pipe.height = canvas.height - this.top_pipe.height - interval
+            this.fly = false
         }
     }
 }
@@ -61,12 +107,21 @@ class Pipe{
 
 //Создание объектов
 const bird = new Bird()
+
+let first_x = canvas.width / 2
+let pipe_one = new Pipe(first_x)
+let pipe_two = new Pipe(first_x + (canvas.width + pipe_one.top_pipe.width) / 2)
 //Игровой цикл
 function loop() {
     ctx.clearRect(0,0, canvas.width, canvas.height)
     bird.draw()
     bird.gravity()
     bird.jump()
+
+    pipe_one.draw()
+    pipe_two.draw()
+    pipe_one.move()
+    pipe_two.move()
     requestAnimationFrame(loop);
 }
 
